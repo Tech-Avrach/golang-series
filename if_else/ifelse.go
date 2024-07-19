9c14a1f4cd3f67dae583d8bc8463bd8502b0b037
Grotti <nikhilpatel9226@gmail.com>
2024-07-19 23:52:16 +0530

    if_else and logical operators

---
+++ if_else/ifelse.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+func main(){
+	// age := 12
+
+	//if else
+
+	// if age >= 18 {
+	// 	fmt.Println("You are adult !")
+	// } else {
+	// 	fmt.Println("Not an adult!")
+	// }
+
+//----------------------------------------------------------------------
+
+	// else if
+
+	// if age >= 18 {
+	// 	fmt.Println("You are adult!")
+	// } else if age >= 12 {
+	// 	fmt.Println("Teenager !")
+	// } else {
+	// 	fmt.Println("Kid !")
+	// }
+
+//----------------------------------------------------------------------
+
+	// logical operators -> &&, ||
+
+	// role := "admin"
+	// hasPermission := true
+
+	// if role == "admin" || hasPermission {
+	// 	fmt.Println("Permission Granted")
+	// }
+
+	// if role == "admin" && hasPermission {
+	// 	fmt.Println("Got Entry")
+	// }
+
+//----------------------------------------------------------------------
+
+	// you can create variable in `if` condition just before writing any condition
+
+	if age := 14; age >= 18 {
+		fmt.Println("Adult !")
+	} else if age >= 12 {
+		fmt.Println("Teenager !")
+	} else {
+		fmt.Println("Kid !")
+	}
+
+//----------------------------------------------------------------------
+
+}
